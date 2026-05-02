20b6397247e8cbf47e21c8b076d7c48698982fd6
Nuri <nuri.bayram@me.com>
2026-05-02 22:59:26 +0200

    Fix base path in vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: '/choicemaker-portal/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
